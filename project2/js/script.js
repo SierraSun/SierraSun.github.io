3f0e71fd2d80746f22060181d3ec0206a1e6379f
sulygirl <31868287+sulygirl@users.noreply.github.com>
2018-03-20 20:15:46 -0400

    delete

--- project2/js/script.js
+++ project2/js/script.js
@@ -65,9 +65,9 @@ $(document).ready(function() {
 			}
 		});
 
-
-		[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) {
-			bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
-		} );
+		//
+		// [].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) {
+		// 	bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
+		// } );
 
 });
